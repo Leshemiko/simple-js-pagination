a6bec66b802cd31ab1093d55506119785911b902
Leshemiko <64403431+Leshemiko@users.noreply.github.com>
2020-09-14 17:33:53 +0300

    uploaded repository files

---
+++ simple-js-pagination.js
@@ -0,0 +1,563 @@
+const places = [
+    {
+      "name": "Place-0",
+      "address": "Address-0",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 1
+    },
+    {
+      "name": "Place-1",
+      "address": "Address-1",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 2
+    },
+    {
+      "name": "Place-2",
+      "address": "Address-2",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 3
+    },
+    {
+      "name": "Place-3",
+      "address": "Address-3",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 4
+    },
+    {
+      "name": "Place-4",
+      "address": "Address-4",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 5
+    },
+    {
+      "name": "Place-5",
+      "address": "Address-5",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 6
+    },
+    {
+      "name": "Place-6",
+      "address": "Address-6",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 7
+    },
+    {
+      "name": "Place-7",
+      "address": "Address-7",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 8
+    },
+    {
+      "name": "Place-8",
+      "address": "Address-8",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 9
+    },
+    {
+      "name": "Place-9",
+      "address": "Address-9",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 10
+    },
+    {
+      "name": "Place-10",
+      "address": "Address-10",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 11
+    },
+    {
+      "name": "Place-11",
+      "address": "Address-11",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 12
+    },
+    {
+      "name": "Place-12",
+      "address": "Address-12",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 13
+    },
+    {
+      "name": "Place-13",
+      "address": "Address-13",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 14
+    },
+    {
+      "name": "Place-14",
+      "address": "Address-14",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 15
+    },
+    {
+      "name": "Place-15",
+      "address": "Address-15",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 16
+    },
+    {
+      "name": "Place-16",
+      "address": "Address-16",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 17
+    },
+    {
+      "name": "Place-17",
+      "address": "Address-17",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 18
+    },
+    {
+      "name": "Place-18",
+      "address": "Address-18",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 19
+    },
+    {
+      "name": "Place-19",
+      "address": "Address-19",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 20
+    },
+    {
+      "name": "Place-20",
+      "address": "Address-20",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Place-21",
+      "address": "Address-21",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 21
+    },
+    {
+      "name": "Place-22",
+      "address": "Address-22",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 22
+    },
+    {
+      "name": "Place-23",
+      "address": "Address-23",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 23
+    },
+    {
+      "name": "Place-24",
+      "address": "Address-24",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 24
+    },
+    {
+      "name": "Place-25",
+      "address": "Address-25",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 25
+    },
+    {
+      "name": "Place-26",
+      "address": "Address-26",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 26
+    },
+    {
+      "name": "Place-27",
+      "address": "Address-27",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 27
+    },
+    {
+      "name": "Place-28",
+      "address": "Address-28",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 28
+    },
+    {
+      "name": "Place-29",
+      "address": "Address-29",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 29
+    },
+    {
+      "name": "Place-30",
+      "address": "Address-30",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 30
+    },
+    {
+      "name": "Place-31",
+      "address": "Address-31",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 31
+    },
+    {
+      "name": "Place-32",
+      "address": "Address-32",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 32
+    },
+    {
+      "name": "Place-33",
+      "address": "Address-33",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 33
+    },
+    {
+      "name": "Place-34",
+      "address": "Address-34",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 34
+    },
+    {
+      "name": "Place-35",
+      "address": "Address-35",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 35
+    },
+    {
+      "name": "Place-36",
+      "address": "Address-36",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 36
+    },
+    {
+      "name": "Place-37",
+      "address": "Address-37",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 37
+    },
+    {
+      "name": "Place-38",
+      "address": "Address-38",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 38
+    },
+    {
+      "name": "Place-39",
+      "address": "Address-39",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 39
+    },
+    {
+      "name": "Place-40",
+      "address": "Address-40",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 40
+    },
+    {
+      "name": "Place-41",
+      "address": "Address-41",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 41
+    },
+    {
+      "name": "Place-42",
+      "address": "Address-42",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 42
+    },
+    {
+      "name": "Place-43",
+      "address": "Address-43",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 43
+    },
+    {
+      "name": "Place-44",
+      "address": "Address-44",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.62146,
+      "lng": 34.60686,
+      "url": "https://www.google.com",
+      "index": 44
+    },
+    {
+      "name": "Place-45",
+      "address": "Address-45",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.699108,
+      "lng": 35.1399,
+      "url": "https://www.google.com",
+      "index": 45
+    },
+    {
+      "name": "Place-46",
+      "address": "Address-46",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.17007,
+      "lng": 34.81423,
+      "url": "https://www.google.com",
+      "index": 46
+    },
+    {
+      "name": "Place-47",
+      "address": "Address-47",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 32.819122,
+      "lng": 34.998386,
+      "url": "https://www.google.com",
+      "index": 47
+    },
+    {
+      "name": "Place-48",
+      "address": "Address-48",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 33.819122,
+      "lng": 35.998386,
+      "url": "https://www.google.com",
+      "index": 48
+    },
+    {
+      "name": "Place-49",
+      "address": "Address-49",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.94554,
+      "lng": 35.22655,
+      "url": "https://www.google.com",
+      "index": 49
+    },
+    {
+      "name": "Place-50",
+      "address": "Address-50",
+      "logo": "https://picsum.photos/100/100",
+      "lat": 31.25327,
+      "lng": 34.40439,
+      "url": "https://www.google.com",
+      "index": 50
+    }
+];
+function Pagination(options, data){
+    let pagination = this;
+    this.settings = {
+        totalItems : data.length,
+        maxItems: options.maxItems || 6,
+        currentPage: options.currentPage || 1,
+        container : options.container || document.querySelector('.data-container'),
+        paginationEl : options.paginationEl || document.querySelector('.pagination'),
+        maxPages : options.maxPages || 5,
+        separator : options.separator || '...',
+        showPrevButton : options.showPrevButton || false,
+        showNextButton : options.showNextButton || false
+    };
+    this.init = function(currentPage){
+        // build pagination
+        this.buildPagination(currentPage);
+        //populate container
+        let counter = 0;
+        if(currentPage === 1){
+            for(var pp = 0; pp < pagination.settings.maxItems; pp++ ){
+                pagination.populateContainer(data[pp], pp);
+            }
+        }else{
+            for( var p = 0; p <= data.length  ; p++){
+                while( p >= (currentPage-1) * pagination.settings.maxItems && counter < pagination.settings.maxItems){
+                    counter++;
+                    if(data[p]){
+                        pagination.populateContainer(data[p], p);
+                    }
+                    break;
+                }
+            }
+        } 
+    };
+
+    this.buildPagination = function(currentPage){
+        let pages = Math.ceil(pagination.settings.totalItems / pagination.settings.maxItems);
+        let prevDisabledClass = (currentPage - 1 < 1 ) ? 'disabled' : '', nextDisabledClass = (currentPage + 1 > pages ) ? 'disabled' : ''; 
+        pagination.settings.container.innerHTML = '';
+        let pagesHTML = '';
+        if( pagination.settings.showPrevButton){
+            pagesHTML+= `<button class="pager pageNav ${prevDisabledClass}" data-page="${currentPage-1}" title="go to prev">></button>`
+        }
+        let pagerCounter = 1;
+        let acc = (currentPage >= pages) ? currentPage - 3 : currentPage - 2;
+        for (var i = acc  ; i <= pages; i++ ){
+            let activeString = (i === currentPage) ? 'active': '';
+            if(i > 0 ){
+                if(pagerCounter <=  pagination.settings.maxPages){
+                    if(currentPage <= pages - 2 ){
+                        if(pagerCounter === pagination.settings.maxPages - 1){
+                            pagesHTML+= `<button class="pager" data-page="${currentPage+1}" title="next">${pagination.settings.separator}</button>`;
+                        }else{
+                            pagesHTML+= `<button class="pager ${activeString}" data-page="${i}" title="go to ${i}">${i}</button>`;
+                        }
+                    }else if((currentPage === (pages - 1) || currentPage === pages)){
+                        if(pagerCounter === 2){
+                            pagesHTML+= `<button class="pager" data-page="${currentPage-1}" title="go to prev">${pagination.settings.separator}</button>`;
+                        }
+                        pagesHTML+= `<button class="pager ${activeString}" data-page="${i}" title="go to ${i}">${i}</button>`;
+                    }else{
+                        if(pagerCounter === currentPage - 1){
+                            pagesHTML+= `<button class="pager" data-page="${currentPage+1}" title="next">${pagination.settings.separator}</button>`;
+                        }
+                        pagesHTML+= `<button class="pager ${activeString}" data-page="${i}" title="go to ${i}">${i}</button>`;
+                    }
+                }
+                pagerCounter++;
+            }
+        };
+        if( pagination.settings.showNextButton){
+            pagesHTML+= `<button class="pager pageNav ${nextDisabledClass}" data-page="${currentPage+1}" title="next"><</button>`
+        }
+        pagination.settings.paginationEl.innerHTML = pagesHTML
+        let pagerButtonElements = document.querySelectorAll('.pager');
+        for(var j = 0; j <= pagerButtonElements.length -1; j++){
+            pagerButtonElements[j].addEventListener('click', function(e){ e.preventDefault(); return pagination.init(parseInt(e.target.getAttribute('data-page') )) } )
+        }
+    };
+
+    this.populateContainer = function(place, markerIndex){
+        let gridItemHTML = ` <div class="institute-grid-item" data-index="${markerIndex}">
+                                <div class="item-logo">
+                                <img src="${place.logo}" alt="${place.name}">
+                                </div>
+                                <div class="item-name">
+                                  ${place.name}
+                                </div>
+                                <div class="item-address">
+                                    ${place.address}
+                                </div>
+                                <div class="item-details">
+                                    <a href="${place.url}" class="empty">Link</a>
+                                </div>
+                            </div>`;
+        pagination.settings.container.innerHTML += gridItemHTML;
+        let gritItems = document.querySelectorAll('.institute-grid-item');
+    };
+};
+window.onload = function(){
+    let pager = new Pagination({},places);
+    pager.init(1)
+};
